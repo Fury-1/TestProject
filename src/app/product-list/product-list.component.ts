25a494b53cc00ef52fff433171ba94d39c10797c
Aakash <furysenpai1@gmail.com>
2022-09-01 16:38:23 +0530

    commit no1

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { products } from '../product';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  products=products
+  constructor() { }
+  share():void
+  {
+    window.alert('The Product has been Shared')
+  }
+  ngOnInit(): void {
+  }
+
+}
